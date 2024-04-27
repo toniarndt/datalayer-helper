9b9121497df71a97e618cc45848c17798bc6cc7e
Toni Arndt <hello@toniarndt.com>
2024-04-27 15:24:05 +0200

    chore: add automatically generated builddate

--- esbuild.js
+++ esbuild.js
@@ -1,4 +1,5 @@
 const { build } = require("esbuild");
+const { definePlugin } = require('esbuild-plugin-define');
 
 const date = new Date();
 const copyright = `/* datalayer-helper (${date.toISOString()}). Copyright ${date.getFullYear()} Toni Arndt. This work is licensed under the terms of the MIT license. For a copy, see <https://github.com/toniarndt/datalayer-helper/blob/main/LICENSE>. */`
@@ -17,6 +18,15 @@ const sharedConfig = {
   },
   tsconfig: './tsconfig.json',
   define: {},
+  plugins: [
+    definePlugin({
+      process: {
+        env: {
+          builddate: date.toISOString(),
+        }
+      },
+    }),
+  ],
 };
 
 build({
